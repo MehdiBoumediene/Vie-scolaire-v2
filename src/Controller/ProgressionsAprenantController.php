59e1220823f3f74ec7b18e3311459821f02776e7
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-07-18 09:31:46 +0200

    frontend: mes notes (apprennant)

--- src/Controller/ProgressionsAprenantController.php
+++ src/Controller/ProgressionsAprenantController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\EtudiantsRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,11 +12,13 @@ class ProgressionsAprenantController extends AbstractController
     /**
      * @Route("/progressions/aprenant", name="app_progressions_aprenant")
      */
-    public function index(): Response
+    public function index(EtudiantsRepository $etudiantsRepository): Response
     {
-        
+        $user= $this->getUser();
+
         return $this->render('progressions_aprenant/index.html.twig', [
             'controller_name' => 'ProgressionsAprenantController',
+            'etudiants' => $etudiantsRepository->findByUser($user),
         ]);
     }
 }
