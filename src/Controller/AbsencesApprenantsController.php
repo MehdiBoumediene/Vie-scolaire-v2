da69ff758ad191e902abcddae3d0a2d3570c6625
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-07-18 08:56:34 +0200

    absences query

--- src/Controller/AbsencesApprenantsController.php
+++ src/Controller/AbsencesApprenantsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use App\Entity\Absences;
 use App\Form\AbsencesType;
 use App\Repository\AbsencesRepository;
+use App\Repository\EtudiantsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,16 +14,19 @@ class AbsencesApprenantsController extends AbstractController
     /**
      * @Route("/absences/apprenants", name="app_absences_apprenants")
      */
-    public function index(AbsencesRepository $absencesRepository): Response
+    public function index(AbsencesRepository $absencesRepository, EtudiantsRepository $etudiantsRepository): Response
     {
         $user = $this->getUser();
+
+        $etudiant = $etudiantsRepository->findOneBy(array('user'=>$user));
+
         $delay = new \Datetime('last month');
         $day = new \Datetime('last day');
 
         return $this->render('absences_apprenants/index.html.twig', [
             'controller_name' => 'AbsencesApprenantsController',
-            'retards' => $absencesRepository->findByUserAbsences($user,$delay,$day),
-            'absences' => $absencesRepository->findByUser($user,$delay,$day),
+            'retards' => $absencesRepository->findByUserAbsences($etudiant,$delay,$day),
+            'absences' => $absencesRepository->findByUser($etudiant,$delay,$day),
 
         ]);
     }
