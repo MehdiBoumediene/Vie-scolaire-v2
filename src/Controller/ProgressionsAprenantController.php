34a26ee72a891bb4d6aa5b41ec31a6c5cf97ece8
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-07-21 13:52:10 +0200

    apprenant profil

--- src/Controller/ProgressionsAprenantController.php
+++ src/Controller/ProgressionsAprenantController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\EtudiantsRepository;
 use App\Repository\NotesRepository;
+use App\Entity\Etudiants;
+use App\Repository\AbsencesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,14 +15,22 @@ class ProgressionsAprenantController extends AbstractController
     /**
      * @Route("/progressions/aprenant", name="app_progressions_aprenant")
      */
-    public function index(EtudiantsRepository $etudiantsRepository,NotesRepository $notesRepository): Response
+    public function index(Etudiants $etudiant, EtudiantsRepository $etudiantsRepository,  AbsencesRepository $absencesRepository, NotesRepository $notesRepository): Response
     {
-        $user= $this->getUser();
-        $etudiant = $etudiantsRepository->findOneBy(array('user'=>$user));
+
+        $delay = new \Datetime('last month');
+        $day = new \Datetime('last day');
+
+        $user = $this->getUser();
+        $classe = $user->getClasse();
 
         return $this->render('progressions_aprenant/index.html.twig', [
             'controller_name' => 'ProgressionsAprenantController',
             'etudiants' => $etudiantsRepository->findBy(array('user'=>$user)),
+            'notes' => $notesRepository->findBy(array('etudiantid'=>$etudiant)),
+            'modules' => $absencesRepository->findBy(array('classe'=>$classe)),
+            'retards' => $absencesRepository->findByUserAbsences($etudiant,$delay,$day),
+            'absences' => $absencesRepository->findByUser($etudiant,$delay,$day),
             'notes' => $notesRepository->findBy(array('etudiantid'=>$etudiant)),
         ]);
     }
